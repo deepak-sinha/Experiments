f7344c1f727d4f9b3b737fa4187344706daec81f
Deepak Sinha <99947497+deepak-sinha@users.noreply.github.com>
2022-02-20 10:30:50 +0530

    Update sketch.js

--- 4/src/scripts/sketch.js
+++ 4/src/scripts/sketch.js
@@ -1,53 +1,52 @@
-let particles;
+let cols;
+let rows;
+let current;
+let previous;
+let dampening = 0.955;
 function setup(){
+	pixelDensity(1);
 	      canvasWidth = window.innerWidth,
       canvasHeight = window.innerHeight;
 
   createCanvas(canvasWidth, canvasHeight);
-  particles=Array(100).fill().map(p=>new Particle());
+  cols = width;
+  rows = height;
+  
+  current = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
+  previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
 
 }
-function draw(){
-	background(0);
-	for(let i = 0;i<9;i++){
-		let p = new Particle();
-        particles.push(p);
-	}
-	for(let particle of particles){
-        particle.update();
-        particle.show();
-	}
-	particles=particles.filter(p=>!p.finished());
-
-	// for(let i =particle.length;i>=0;i--){
-	// 	particles[i].update();
-	// 	particles[i].show();
-	// 	if(particles[i].finished()){
-	// 		particles.splice(i,1);
-	// 	}
-	// }
-	
+function mousePressed() {
+  previous[mouseX][mouseY] = 2000;
+}
+function mouseDragged() {
+  previous[mouseX][mouseY] = 2000;
 }
 
-class Particle{
-	constructor(){
-		this.x=width/2;
-		this.y=height-100;
-		this.vx=random(-1.5,1.5);
-		this.vy=random(-6,1);
-		this.alpha=400;
-	}
-	finished(){
-		return this.alpha<0;
-	}
-	update(){
-		this.x+=this.vx;
-		this.y+=this.vy;
-        this.alpha-=5;
-	}
-	show(){
-		noStroke();
-		fill(255,0,0);
-		ellipse(this.x,this.y,random(2,15));
-	}
+
+function draw() {
+  background(0);
+  loadPixels();
+  for (let i = 1; i < cols - 1; i++) {
+    for (let j = 1; j < rows - 1; j++) {
+      current[i][j] =
+        (previous[i - 1][j] +
+          previous[i + 1][j] +
+          previous[i][j - 1] +
+          previous[i][j + 1]) /
+          2 -
+        current[i][j];
+      current[i][j] = current[i][j] * dampening;
+   
+      let index = (i + j * cols) * 4;
+      pixels[index + 0] = current[i][j];
+      pixels[index + 1] = current[i][j];
+      pixels[index + 2] = current[i][j];
+    }
+  }
+  updatePixels();
+
+  let temp = previous;
+  previous = current;
+  current = temp;
 }
